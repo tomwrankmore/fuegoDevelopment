3b752c386a880a0c27575a728df6550580c13e9c
Blixt <teodor.blixt@gmail.com>
2019-04-21 21:18:25 +0100

    changed header to header component and added styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import sanityClient from '../Client';
+import imageUrlBuilder from '@sanity/image-url';
+import '../css/Header.css'
+
+const builder = imageUrlBuilder(sanityClient)
+function urlFor(source) {
+  return builder.image(source)
+}
 
 
-const Header = (email) => {
+const Header = ({email, phone, logo, menu}) => {
+
   return (
     <div className="header">
       <div className="contact">
-        <p>{email}</p>
+        <a href={`mailto:${email}`}>{email}</a>
+        <a href={`tel:${phone}`}>{phone}</a>
       </div>
+        <a href='#'><img alt="Logo" src={urlFor(logo).width(500).url()}/></a>
+        <ul className="menu">
+          {menu.map((menu, _id) => {
+            return (
+                <li><a href="#">{menu.name}</a></li>
+            )
+          })}
+        </ul>
     </div>
   )
 }
